fdcce7779e24ef2785d3cf6005af24077bc51770
Dieter Heiss <weboss@gmail.com>
2023-02-11 00:02:32 -0300

    Commit path

--- README.md
+++ README.md
@@ -10,3 +10,17 @@ Caso seja a primeira vez que utilize o Venv, provavelmente vai dar um erro.
 
 Abra o Powershell e modo Admin e execute o seguinte comando: Set-ExecutionPolicy -Executionpolicy AllSigned
 Opção A. 
+
+Lição - 10:
+
+Path parameters:  "{id}"
+
+@app.get('/blog/{id}')
+def index(id):
+	return("message": f"Blog with id {id}"}
+#Teste o resultado assim: http://127.0.0.1:8000/blog/65
+#Deverá aparecer o num 65
+
+Predefined values
+
+Query parameters
--- Tutorial/main.py
+++ Tutorial/main.py
@@ -2,7 +2,16 @@ from fastapi import FastAPI
 
 app = FastAPI ()
 
-
-@app.get('/')
+@app.get('/hello')
 def index():
-    return 'Hello World!'
+    return {'message' : 'Hello world!'}
+
+#A ordem dos fatores alteram o resultado. Como o próximo resulta do Path, ele interfere caso ('/blog/all') esteja depois 
+@app.get('/blog/all')
+def get_all_blogs():
+    return {'message':'Tudo do site'}
+
+#Teste: http://127.0.0.1:8000/blog/65
+@app.get('/blog/{id}')
+def get_blog(id: int):
+    return {'message' : f'Blog com id {id}!'}
